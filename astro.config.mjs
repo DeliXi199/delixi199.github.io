666315ba4102894c19cfc70501b7f4bf701de010
DeliXi <3477630483dali@gmail.com>
2025-01-26 15:04:35 +0800

    set website in github

--- astro.config.mjs
+++ astro.config.mjs
@@ -24,7 +24,8 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: USER_SITE,
+  // site: USER_SITE,
+  site: "https://github.com/DeliXi199/delixi199.github.io/",
   output: "static",
   style: {
     scss: {
